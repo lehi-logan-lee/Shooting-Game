5f70d7c2395d2b5bdd2edbf0d7a12882a3c8bfa8
lehi-logan-lee <33855652+lehi-logan-lee@users.noreply.github.com>
2017-11-21 15:12:06 +0900

    Add files via upload

---
+++ src/main/java/com/example/student/shootinggame/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.student.shootinggame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+        getWindow().clearFlags(
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new ShootingView(this));
+    }
+}
